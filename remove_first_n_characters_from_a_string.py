6b8c6a760f790802939f8c63d9cd33a13d19fc81
unknown <centinaraphael0157@gmail.com>
2024-01-25 16:13:29 +0800

    Code upload

---
+++ remove_first_n_characters_from_a_string.py
@@ -0,0 +1,11 @@
+import random
+print('This is a code where a random amount of a given word will disappear')
+user_input_word = input('Can you give me a word ')
+random_amount_to_be_eaten = random.randint(1, len(str(user_input_word)+1))
+
+def word_eater(word, amount_to_be_eaten):
+    print('Your inputted word is:', user_input_word)
+    new_word = user_input_word[amount_to_be_eaten: ]
+    return new_word
+
+print(word_eater(user_input_word,random_amount_to_be_eaten))
