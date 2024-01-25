e9c40914b40b90c06fc88e0444f75883c14510dc
unknown <centinaraphael0157@gmail.com>
2024-01-25 16:17:10 +0800

    Decided that it will be better if it will eat at the least, one letter

--- remove_first_n_characters_from_a_string.py
+++ remove_first_n_characters_from_a_string.py
@@ -1,7 +1,7 @@
 import random
 print('This is a code where a random amount of a given word will disappear')
 user_input_word = input('Can you give me a word ')
-random_amount_to_be_eaten = random.randint(0, len(str(user_input_word)))
+random_amount_to_be_eaten = random.randint(1, len(str(user_input_word)))
 
 def word_eater(word, amount_to_be_eaten):
     print('Your inputted word is:', user_input_word)
